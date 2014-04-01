567ddb63d0fdb161b430d9b36a7a87592f0eb0e9
Adrian Miranda <adriancmiranda@gmail.com>
2014-04-01 09:47:35 -0300

    Update index.js

--- app/index.js
+++ app/index.js
@@ -6,26 +6,26 @@ var chalk = require('chalk');
 
 
 var GulpRequirejsGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
+	init: function () {
+		this.pkg = require('../package.json');
 
-    this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
-    });
-  },
+		this.on('end', function () {
+			if (!this.options['skip-install']) {
+				this.installDependencies();
+			}
+		});
+	},
 
-  askFor: function () {
-    var done = this.async();
+	askFor: function () {
+		var done = this.async();
 
-    // have Yeoman greet the user
-    this.log(this.yeoman);
+		// have Yeoman greet the user
+		this.log(this.yeoman);
 
-    // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('You\'re using the fantastic GulpRequirejs generator.'));
+		// replace it with a short and sweet description of your generator
+		this.log(chalk.magenta('You\'re using the fantastic GulpRequirejs generator.'));
 
-    var prompts = [{
+		var prompts = [{
 		type: 'checkbox',
 		name: 'features',
 		message: 'What more would you like?',
@@ -35,58 +35,58 @@ var GulpRequirejsGenerator = yeoman.generators.Base.extend({
 			value: 'includeModernizr',
 			checked: true
 		}]
-    }];
+		}];
 
-    this.prompt(prompts, function (answers) {
+		this.prompt(prompts, function (answers) {
 		var features = answers.features;
 		this.includeModernizr = features;
 		done();
-    }.bind(this));
-  },
+		}.bind(this));
+	},
 
-  app: function () {
-    this.mkdir('app');
-	this.mkdir('app/fonts');
-	this.mkdir('app/scripts');
-	this.mkdir('app/images');
-	this.mkdir('app/theme');
-	this.mkdir('app/theme/base');
-	this.mkdir('app/theme/layout');
-	this.mkdir('app/theme/modules');
-	this.mkdir('app/theme/states');
+	app: function () {
+		this.mkdir('app');
+		this.mkdir('app/fonts');
+		this.mkdir('app/scripts');
+		this.mkdir('app/images');
+		this.mkdir('app/theme');
+		this.mkdir('app/theme/base');
+		this.mkdir('app/theme/layout');
+		this.mkdir('app/theme/modules');
+		this.mkdir('app/theme/states');
 
-    this.copy('_gulpfile.js', 'gulpfile.js');
-    this.copy('_package.json', 'package.json');
-    this.copy('_bower.json', 'bower.json');
-  },
+		this.copy('_gulpfile.js', 'gulpfile.js');
+		this.copy('_package.json', 'package.json');
+		this.copy('_bower.json', 'bower.json');
+	},
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
-    this.copy('gitattributes', '.gitattributes');
-    this.copy('gitignore', '.gitignore');
-    this.copy('jshintrc', '.jshintrc');
-    this.copy('jscs.json', '.jscs.json');
-    this.copy('bowerrc', '.bowerrc');
-    this.copy('config.js', 'app/scripts/config.js');
-    this.copy('scope.js', 'app/scripts/scope.js');
-    this.copy('app.js', 'app/scripts/app.js');
-    this.copy('favicon.ico', 'app/favicon.ico');
-    this.copy('_reset.scss', 'app/theme/base/_reset.scss');
-    this.copy('_mixins.scss', 'app/theme/base/_mixins.scss');
-    this.copy('_variables.scss', 'app/theme/base/_variables.scss');
-    this.copy('_main.scss', 'app/theme/layout/_main.scss');
-    this.copy('_menu.scss', 'app/theme/modules/_menu.scss');
-    this.copy('_mediaqueries.scss', 'app/theme/states/_mediaqueries.scss');
-	this.copy('_base.scss', 'app/theme/_base.scss');
-	this.copy('_layout.scss', 'app/theme/_layout.scss');
-	this.copy('_modules.scss', 'app/theme/_modules.scss');
-	this.copy('_states.scss', 'app/theme/_states.scss');
-    this.copy('theme.scss', 'app/theme.scss');
-    this.copy('index.html', 'app/index.html');
-    this.copy('htaccess', 'app/.htaccess');
-    this.copy('robots.txt', 'app/robots.txt');
-    this.copy('humans.txt', 'app/humans.txt');
-  }
+	projectfiles: function () {
+		this.copy('editorconfig', '.editorconfig');
+		this.copy('gitattributes', '.gitattributes');
+		this.copy('gitignore', '.gitignore');
+		this.copy('jshintrc', '.jshintrc');
+		this.copy('jscs.json', '.jscs.json');
+		this.copy('bowerrc', '.bowerrc');
+		this.copy('config.js', 'app/scripts/config.js');
+		this.copy('scope.js', 'app/scripts/scope.js');
+		this.copy('app.js', 'app/scripts/app.js');
+		this.copy('favicon.ico', 'app/favicon.ico');
+		this.copy('_reset.scss', 'app/theme/base/_reset.scss');
+		this.copy('_mixins.scss', 'app/theme/base/_mixins.scss');
+		this.copy('_variables.scss', 'app/theme/base/_variables.scss');
+		this.copy('_main.scss', 'app/theme/layout/_main.scss');
+		this.copy('_menu.scss', 'app/theme/modules/_menu.scss');
+		this.copy('_mediaqueries.scss', 'app/theme/states/_mediaqueries.scss');
+		this.copy('_base.scss', 'app/theme/_base.scss');
+		this.copy('_layout.scss', 'app/theme/_layout.scss');
+		this.copy('_modules.scss', 'app/theme/_modules.scss');
+		this.copy('_states.scss', 'app/theme/_states.scss');
+		this.copy('theme.scss', 'app/theme.scss');
+		this.copy('index.html', 'app/index.html');
+		this.copy('htaccess', 'app/.htaccess');
+		this.copy('robots.txt', 'app/robots.txt');
+		this.copy('humans.txt', 'app/humans.txt');
+	}
 });
 
 module.exports = GulpRequirejsGenerator;
