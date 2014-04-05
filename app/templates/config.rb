2f64c9e4e8ed5295116d7bd79bc492bcb44d00b6
Adrian Miranda <adriancmiranda@gmail.com>
2014-04-05 01:34:24 -0300

    Create config.rb

---
+++ app/templates/config.rb
@@ -0,0 +1,7 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# @see http://compass-style.org/help/tutorials/configuration-reference/
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+http_path = '/'
+images_dir = 'app/images'
+javascripts_dir = 'app/scripts'
+relative_assets = true
