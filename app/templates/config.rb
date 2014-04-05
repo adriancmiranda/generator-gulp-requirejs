821a5e1ed14cefb9108d3ad4a4ff60537e07e506
Adrian Miranda <adriancmiranda@gmail.com>
2014-04-05 01:39:51 -0300

    Update config.rb

--- app/templates/config.rb
+++ app/templates/config.rb
@@ -2,6 +2,7 @@
 # @see http://compass-style.org/help/tutorials/configuration-reference/
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 http_path = '/'
+fonts_dir = 'app/fonts'
 images_dir = 'app/images'
 javascripts_dir = 'app/scripts'
 relative_assets = true
