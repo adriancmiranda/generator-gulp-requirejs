0b388cd18153d3d4fab168997fad6b50fab0d621
Adrian Miranda <adriancmiranda@gmail.com>
2014-04-10 00:43:12 -0300

    Delete _config.rb

--- app/templates/_config.rb
+++
@@ -1,8 +0,0 @@
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# @see http://compass-style.org/help/tutorials/configuration-reference/
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-http_path = '/'
-fonts_dir = 'app/fonts'
-images_dir = 'app/images'
-javascripts_dir = 'app/scripts'
-relative_assets = true
