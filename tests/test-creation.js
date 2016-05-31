38f7f21b1cace86a3e5f033179ac56fac1325fb1
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-05-31 01:53:54 -0300

    Update: spec

--- tests/test-creation.js
+++ tests/test-creation.js
@@ -3,6 +3,7 @@
 
 var path = require('path');
 var helpers = require('yeoman-generator').test;
+var Utils = require('../generators/utils');
 
 describe('gulp-requirejs generator', function(){
 	beforeEach(function(done){
