5dd888039d605423cc0c78311bf2d448c499eb7b
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-05-31 01:36:36 -0300

    Update: spec

--- generators/app/index.js
+++ generators/app/index.js
@@ -19,8 +19,8 @@ var FullStackGenerator = new Proto(yogen.Base, {
     this.pack = Utils.readJsonSync(path.join(__dirname, '../../package.json'));
     this.name = this.pack.name.replace(/^generator\-/, '');
     this.log(yosay(chalk.magenta('You\'re using the fantastic', chalk.yellow('Gulp\\RequireJS') + chalk.magenta(' fullstack generator.'))));
-		this.composeWith(this.name +':front', { options:this.options, args:this.args });
     this.composeWith(this.name +':back', { options:this.options, args:this.args });
+		this.composeWith(this.name +':front', { options:this.options, args:this.args });
 	}
 });
 
