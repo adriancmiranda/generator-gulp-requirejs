53f70c68c45ef6b13e6ec493679bd4a699d8fb4e
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-05-31 01:35:07 -0300

    Update: spec

--- tasks/jslint.js
+++ tasks/jslint.js
@@ -1,6 +1,6 @@
 module.exports = function(gulp, data, util){
 	'use strict';
-	
+
 	// The classic and strict javascript lint-tool for gulp.js.
 	// @see https://www.npmjs.com/package/gulp-jslint
 	var jslint = require('gulp-jslint');
@@ -10,7 +10,6 @@ module.exports = function(gulp, data, util){
 		var tests = util.path.join(data.appDirs.tests, '**/*.js');
 		return gulp.src([tests, scripts])
 			.pipe(jslint())
-			.pipe(jslint.reporter('default'))
 		;
 	};
 };
