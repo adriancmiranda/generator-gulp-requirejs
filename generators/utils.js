b4e6a696c072d5cf88e5cffd7cf80db5117af40e
Adrian C. Miranda <adriancmiranda@gmail.com>
2016-05-31 23:05:41 -0300

    Update: utils

--- generators/utils.js
+++ generators/utils.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var slugify = require('underscore.string/slugify');
 var path = require('path');
 var fsx = require('fs-extra');
 
@@ -13,6 +14,9 @@ exports.hasFeature = function(list, feature){
 	return !!~(Array.isArray(list)? list : []).indexOf(feature);
 };
 
-exports.appname = function(){
+exports.appname = function(value){
+	if(value){
+		return slugify(value);
+	}
 	return path.basename(process.cwd()).replace(/[^\w\s]+?/g, ' ');
 };
